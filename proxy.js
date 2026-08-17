435010dcbc58e08596f381a53a1b4fedd3cd9adf
einlee0130 <einlee0130@gmail.com>
2026-08-17 09:16:00 +0000

    Fix authentication redirect

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export function middleware(request) {
+export function proxy(request) {
   const auth = request.cookies.get('phonebook_auth')?.value
 
   if (auth !== 'true') {
